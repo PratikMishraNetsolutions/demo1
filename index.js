578812f93a25766e51793a889a0cdf682661c37a
PratikMishraNetsolutions <pratik.mishra@netsolutions.com>
2023-10-10 19:10:56 +0530

    working on the implementation of async and await

---
+++ index.js
@@ -0,0 +1,13 @@
+// const p = new Promise((resolve,reject)=>{
+//     resolve("my name is pratik")
+// })
+
+
+// async function getData(){
+//     return p;
+// }
+// const dataPromise = getData()
+ 
+// dataPromise.then(result=> console.log(result))
+    
+
